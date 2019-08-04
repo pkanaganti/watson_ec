d8b50ba66cda03025b2e4cd1b45906548dc5828b
pkanaganti <pkanaganti@unomaha.edu>
2019-08-03 23:30:39 -0500

    watson extra assignment

---
+++ blog/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+import json
+#from ibm_watson import ToneAnalyzerV3
+
+import requests
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud.tone_analyzer_v3 import ToneInput
+from watson_developer_cloud import LanguageTranslatorV3
+
+language_translator = LanguageTranslatorV3(
+        version='2018-05-31',
+        iam_apikey='PckvhDE1FkqfzN8ajFYoRa_RUSE8otYsnNrJGR_hzey_')
+
+
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    iam_apikey='2h8D2iJJPfigBXh76q7d0V1YJgvaGM7_ItHXx6INYBJA',
+)
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    for post in posts:
+        posting = post.text
+
+        translation = language_translator.translate(
+            text=post.text, model_id='en-es').get_result()
+        obj = (json.dumps(translation, indent=2, ensure_ascii=False))
+        print(obj)
+        obj2 = json.loads(obj)
+        post.obj2 = obj2['translations'][0]['translation']
+        post.w_count = obj2['word_count']
+        post.c_count = obj2['character_count']
+        tone_analysis = tone_analyzer.tone(
+            {'text': posting},
+            content_type='application/json'
+        ).get_result()
+        tone2 = str(tone_analysis)
+        post.tone3 = (tone2[1:500])
+        print(post.tone3)
+        print(json.dumps(tone_analysis, indent=2))
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
